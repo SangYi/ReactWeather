f4cc72cba4c2572a780a19b4d02f73b372778179
Sang Yi <Sang Yi>
2016-06-21 09:02:17 -0500

    Added foundation & jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const webpack = require('webpack');
 const path = require('path');
 
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins:[
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: path.join(__dirname, "/public/"),
     filename: 'bundle.js'
