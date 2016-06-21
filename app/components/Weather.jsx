4b094e976d8ffc6378f1fe3a80cd548d3da7fd67
Sang Yi <Sang Yi>
2016-06-21 12:22:23 -0500

    added error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
   getInitialState: function(){
@@ -11,28 +12,42 @@ var Weather = React.createClass({
   },
   handleSearch: function(location){
     var that = this;
-    this.setState({isLoading: true});
+
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
     openWeatherMap.getTemp(location).then(function(temp){
       that.setState({
         location:location,
         temp: temp,
         isLoading: false
       });
-    }, function(errorMessage){
-      that.setState({isLoading:false});
-      alert(errorMessage);
+    }, function(e){
+      that.setState({
+        isLoading:false,
+        errorMessage: e.message
+      });
     });
 
   },
 
   render: function() {
-    var {isLoading, location, temp} = this.state;
+    var {isLoading, location, temp, errorMessage} = this.state;
 
     function renderMessage(){
       if(isLoading){
         return <h3 className="text-center">Fetching weather...</h3>;
       }else if(temp && location) {
         return <WeatherMessage location={location} temp={temp}/>;
+      }
+    }
+
+    function renderError () {
+      if(typeof errorMessage === 'string'){
+        return(
+          <ErrorModal/>
+        )
       }
     }
 
